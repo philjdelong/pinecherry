9ca76c825fcc57ce60090194c13c0e622a054590
philjdelong <philjdelong@gmail.com>
2020-08-26 14:04:20 -0600

    Initial commit

---
+++ routes/base.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({ status: "OK", code: 200 });
+    console.log("ET Node Home.")
+});
+
+module.exports = router;
---
+++ routes/init.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/init', (req, res) => {
+    res.json({ status: 'OK', code: 200 });
+    console.log("We're init..")
+});
+
+module.exports = router;
